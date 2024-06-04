80c2f09e2e60c018c8e5bc95ee58438b17ec7aff
Uday Gajera <109271857+udayg24@users.noreply.github.com>
2024-06-04 14:48:58 +0530

    initial commit

--- Package.swift
+++ Package.swift
@@ -11,11 +11,13 @@ let package = Package(
             name: "VirtualBackgroundVideoSDK",
             targets: ["VirtualBackgroundVideoSDK"]),
     ],
+    dependencies: [.package(url: "https://github.com/stasel/WebRTC.git", from: Version(stringLiteral: "125.0.0"))],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "VirtualBackgroundVideoSDK"),
+            name: "VirtualBackgroundVideoSDK",
+        dependencies: ["WebRTC"]),
         .testTarget(
             name: "VirtualBackgroundVideoSDKTests",
             dependencies: ["VirtualBackgroundVideoSDK"]),
--- Sources/VirtualBackgroundVideoSDK/VirtualBackgroundVideoSDK.swift
+++ Sources/VirtualBackgroundVideoSDK/VirtualBackgroundVideoSDK.swift
@@ -1,2 +1,137 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import ARKit
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import WebRTC
+
+public protocol VideoSDKVideoProcessor {
+     func onFrameReceived(frame: RTCVideoFrame) -> RTCVideoFrame?
+}
+
+public class VideoSDKBackgroundProcessor: VideoSDKVideoProcessor {
+
+    private var backgroundci: CIImage?
+    
+    public init(backgroundSource: URL) {
+        downloadImage(from: backgroundSource){ image in
+            if let image = image {
+                self.backgroundci = image
+            } else {
+                print("Error downloading image")
+            }
+        }
+    }
+    
+    public func onFrameReceived(frame: RTCVideoFrame) -> RTCVideoFrame? {
+        let buffer = frame.buffer as! RTCCVPixelBuffer
+        let pixelBuffer = buffer.pixelBuffer
+        
+        // Perform person segmentation
+        if #available(iOS 15.0, *) {
+            let request = VNGeneratePersonSegmentationRequest()
+            request.qualityLevel = .balanced
+            let requestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:])
+            
+            do {
+                try requestHandler.perform([request])
+                //                    request.qualityLevel = .accurate
+                request.outputPixelFormat = kCVPixelFormatType_OneComponent8
+                guard let result = request.results?.first as? VNPixelBufferObservation else {
+                    return nil
+                }
+                
+                let maskPixelBuffer = result.pixelBuffer
+                // Composite the image with the virtual background
+                if let compositedPixelBuffer = compositeImage(originalPixelBuffer: pixelBuffer, maskPixelBuffer: maskPixelBuffer) {
+                    // Create RTCVideoFrame from the composited pixel buffer
+                    let rtcPixelBuffer = RTCCVPixelBuffer(pixelBuffer: compositedPixelBuffer)
+                    let rtcVideoFrame = RTCVideoFrame(buffer: rtcPixelBuffer, rotation: frame.rotation, timeStampNs: frame.timeStampNs)
+                    //                self.meeting?.sendProcessedFrame(frame: rtcVideoFrame)
+                    return rtcVideoFrame;
+                } else {
+                }
+            } catch {
+                print("Error performing person segmentation request: \(error)")
+            }
+        }
+        return nil
+    }
+    
+    public func changeBackground(backgroundSource: URL) {
+        downloadImage(from: backgroundSource) { image in
+            if let image = image {
+                self.backgroundci = image
+            }
+            else {
+                return
+            }
+        }
+    }
+    
+    @available(iOS 13.0, *)
+    func compositeImage(originalPixelBuffer: CVPixelBuffer, maskPixelBuffer: CVPixelBuffer) -> CVPixelBuffer? {
+        let ciImage = CIImage(cvPixelBuffer: originalPixelBuffer)
+        let maskCIImage = CIImage(cvPixelBuffer: maskPixelBuffer)
+        
+        let maskScaleX = ciImage.extent.width / maskCIImage.extent.width
+        let maskScaleY = ciImage.extent.height / maskCIImage.extent.height
+        let maskScaled =  maskCIImage.transformed(by: __CGAffineTransformMake(maskScaleX, 0, 0, maskScaleY, 0, 0))
+        
+        guard let backgroundCIImage = self.backgroundci else {
+            return nil
+        }
+        let backgroundScaleX = ciImage.extent.width / backgroundCIImage.extent.width
+        let backgroundScaleY = ciImage.extent.height / backgroundCIImage.extent.height
+        
+        let backgroundScaled =  backgroundCIImage.transformed(by: __CGAffineTransformMake(backgroundScaleX, 0, 0, backgroundScaleY, 0, 0))
+        
+        //        let filter = CIFilter(name: "CIBlendWithMask")
+        //
+        ////        filter?.setValue(ciImage, forKey: kCIInputImageKey)
+        //        filter?.setValue(maskCIImage, forKey: kCIInputMaskImageKey)
+        //        filter?.setValue(backgroundCIImage, forKey: kCIInputBackgroundImageKey)
+        //
+        //        guard let outputImage = filter?.outputImage else { return nil }
+        
+        let blendFilter = CIFilter.blendWithMask()
+        blendFilter.inputImage = ciImage
+        blendFilter.backgroundImage = backgroundScaled
+        blendFilter.maskImage = maskScaled
+        
+        let blendedImage = blendFilter.outputImage
+        let ciContext = CIContext(options: nil)
+        let filteredImageRef = ciContext.createCGImage(blendedImage!, from: blendedImage!.extent)
+        let maskDisplayRef = ciContext.createCGImage(maskScaled, from: maskScaled.extent)
+        
+        var outputPixelBuffer: CVPixelBuffer?
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+             kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        let width = Int(blendedImage!.extent.width)
+        let height = Int(blendedImage!.extent.height)
+        
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, width, height, kCVPixelFormatType_32BGRA, attrs, &outputPixelBuffer)
+        
+        guard status == kCVReturnSuccess, let buffer1 = outputPixelBuffer else {
+            return nil
+        }
+        ciContext.render(blendedImage!, to: buffer1)
+        return buffer1
+    }
+    
+    func downloadImage(from url: URL, completion: @escaping (CIImage?) -> Void) {
+        DispatchQueue.global().async {
+            do {
+                let imageData = try Data(contentsOf: url)
+                let image = CIImage(data: imageData)
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+            } catch {
+                completion(nil)
+            }
+        }
+    }
+}
